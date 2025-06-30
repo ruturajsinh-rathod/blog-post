e3215fbd78c79e92ddb60a61c3ba59b49ea872f6
Ruturajsinh Rathod <ruturajsinh.rathod@mindinventory.com>
2025-06-30 14:59:26 +0530

    removed custom commands for alembic

--- migrations/env.py
+++ migrations/env.py
@@ -5,10 +5,12 @@ from alembic import context
 from sqlalchemy.engine import Connection
 
 from apps import Base
+from config import settings
 from core.db import engine
 
 
 config = context.config
+config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
--- src/main.py
+++ src/main.py
@@ -1,4 +1,31 @@
-from cli import cli
+from typing import Optional
+import uvicorn
+
+from config import settings
+
+
+def run(
+    host: Optional[str] = None,
+    port: Optional[int] = None,
+) -> None:
+    """
+    Run the server.
+    """
+    if not host:
+        host = settings.APP_HOST
+    if not port:
+        port = settings.APP_PORT
+
+    uvicorn.run(
+        "apps.server:debug_app",
+        host=host,
+        port=port,
+        reload=True,
+        reload_dirs=["."],
+        log_level="debug",
+        use_colors=True,
+    )
+
 
 if __name__ == "__main__":
-    cli()
+    run()
